231fb960ec6a3ed051cb43c18574a412a9616f42
Alexandr <menal2016@mail.ru>
2021-05-17 18:27:46 +0700

    Create function Which checks the entered characters for those that can be entered according to the TK

--- src/libtodolist/task.c
+++ src/libtodolist/task.c
@@ -1,24 +1,33 @@
 #include "task.h"
 #include <stdio.h>
+bool check_characters_by_ASCII(selected_list* sl, FILE* file)
+{
+    int i = 0;
+    while (sl->name_task[i] != '\0') {
+        if (!((sl->name_task[i] >= ' ' && sl->name_task[i] <= '}'))) {
+            return false;
+            break;
+        }
+        ++i;
+    }
+    return true;
+}
 void create_task(selected_list* sl, FILE* file)
 {
     printf("Always enter X as the first character, f as the second task number "
            "and period");
     fgets(sl->name_task, 150, stdin);
-    int i = 0;
-    while (sl->name_task[i] != '\0') {
-        if (!((sl->name_task[i] >= ' ' && sl->name_task[i] <= '}'))) {
-            printf("Repeat again\n");
-            break;
-        }
-        ++i;
-    }
     /*
     int number_task;
     printf("Enter the task number\n");
     scanf("%d", &number_task);
     printf("%d.", number_task);*/
-    fwrite(sl->name_task, sizeof(char), 151, file);
+    if (!check_characters_by_ASCII(sl, file)) {
+        printf("You have entered a symbol that our program does not support, "
+               "please try again");
+    } else {
+        fwrite(sl->name_task, sizeof(char), 151, file);
+    }
 }
 int search_last_task(selected_list* sl, FILE* file)
 {
--- src/libtodolist/task.h
+++ src/libtodolist/task.h
@@ -2,7 +2,10 @@
 #include "selected_list_type.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 
+bool check_characters_by_ASCII(selected_list* sl, FILE* file);
 void create_task(selected_list* sl, FILE* file);
 int search_last_task(selected_list* sl, FILE* file);
 void delete_task(selected_list* sl, FILE* file);
