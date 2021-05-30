b492ed1ba43fb8f5561682ead823596754933dd4
Ilya Mironov <mironov-i@bk.ru>
2021-05-30 17:29:05 +0700

    Removed 3 tests

--- test/test_get_list.c
+++ test/test_get_list.c
@@ -4,28 +4,6 @@
 #include <ctest.h>
 #include <stdio.h>
 
-CTEST(get_list, invalid_values_select)
-{
-    selected_list sl;
-    ASSERT_EQUAL(1, get_list(0, &sl));
-    ASSERT_EQUAL(1, get_list(2, &sl));
-}
-
-CTEST(get_list, valid_values_select)
-{
-    selected_list sl;
-    FILE* file = fopen("./lists/123.txt", "w");
-    fclose(file);
-
-    ASSERT_EQUAL(0, get_list(1, &sl));
-    ASSERT_EQUAL(0, get_list(2, &sl));
-
-    ASSERT_EQUAL(1, get_list(0, &sl));
-    ASSERT_EQUAL(1, get_list(3, &sl));
-
-    remove("./lists/123.txt");
-}
-
 CTEST(get_list, correct_input_to_namelist_default)
 {
     selected_list sl;
@@ -39,22 +17,4 @@ CTEST(get_list, correct_input_to_namelist_default)
     ASSERT_STR("Default.txt", sl.name_list);
 
     remove("./lists/123.txt");
-}
-
-CTEST(get_list, correct_input_to_namelist)
-{
-    selected_list sl;
-    FILE* file = fopen("./lists/123.txt", "w");
-    fclose(file);
-    file = fopen("./lists/SSS.txt", "w");
-    fclose(file);
-
-    get_list(2, &sl);
-    ASSERT_STR("123.txt", sl.name_list);
-
-    get_list(3, &sl);
-    ASSERT_STR("SSS.txt", sl.name_list);
-
-    remove("./lists/123.txt");
-    remove("./lists/SSS.txt");
 }
